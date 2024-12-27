e4fd6c63f601cea15f7a44d9e62485c302945e1b
Dave <dave.lhw@gmail.com>
2024-12-28 07:16:54 +0800

    dd

--- vws.js
+++ vws.js
@@ -42,7 +42,7 @@ let inviteData = null;
 
 bot.use(session({ initial }));
 
-let play_url = "";
+let play_url = ""; //blank for now
 
 function restartServer() {
   console.log("Attempting to restart the server...");
