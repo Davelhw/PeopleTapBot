17045e03987f4b75bc5d061efd364085dfb21cf7
Dave <dave.lhw@gmail.com>
2024-12-28 06:56:30 +0800

    remark s3 code

--- vws.js
+++ vws.js
@@ -17,11 +17,11 @@ const path = require("path");
 require("dotenv").config();
 
 // AWS S3 Configuration
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
-});
+// const s3 = new AWS.S3({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: process.env.AWS_REGION
+// });
 
 // Initialize express app
 const app = express();
